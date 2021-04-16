af377ecc095191f5641641322ffbfc6f8b2c45c4
SasLeo <saskia.kutz@fu-berlin.de>
2021-04-16 11:15:35 +0200

    packages are now imported from a separate file

--- pythonr/internal_bayesian.R
+++ pythonr/internal_bayesian.R
@@ -1,30 +1,3 @@
-
-  if (!require(pacman, quietly = TRUE))
-    install.packages("pacman")
-  # if (!require(devtools))
-  #   install.packages("devtools")
-  # if (!require(RSMLM))
-  #   pacman::p_load("devtools")
-  #   install_github("lucabaronti/RSMLM")
-  pacman::p_load(
-    "dplyr",
-    "pryr",
-    "ggpubr",
-    "splancs",
-    "igraph",
-    "RSMLM",
-    "tictoc",
-    "geometry",
-    "doParallel",
-    "beepr",
-    "data.table",
-    "plyr",
-    "ggforce",
-    "tidyverse",
-    "rhdf5"
-  )
-  pacman::p_load_gh("saskiakutz/RSMLM")
-
 mcgaussprec <- function(pts,
                         sds,
                         xlim = c(0, 1),
