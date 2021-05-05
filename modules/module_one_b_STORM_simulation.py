98ffd5ea229ced2875e97cc706fdda27178a1bae
SasLeo <saskia.kutz@fu-berlin.de>
2021-05-05 17:36:06 +0200

    connected cancel button to cancel action (new function on_cancel)

--- modules/module_one_b_STORM_simulation.py
+++ modules/module_one_b_STORM_simulation.py
@@ -17,36 +17,43 @@ class MainWindow_simulation_STORM(qtw.QWidget):
         # Main UI code goes here
 
         self.model = Model_STORM()
-        self.view = ViewSTORM()
+        self.storm_view = ViewSTORM()
         self.setLayout(qtw.QVBoxLayout())
-        self.layout().addWidget(self.view)
+        self.layout().addWidget(self.storm_view)
 
         self.sim_STORM_thread = qtc.QThread()
         self.model.moveToThread(self.sim_STORM_thread)
         self.model.finished.connect(self.sim_STORM_thread.quit)
         self.sim_STORM_thread.start()
 
-        self.view.submitted.connect(self.model.set_data)
-        self.view.start_STORM.connect(self.sim_STORM_thread.start)
-        self.view.start_STORM.connect(self.on_started)
-        self.view.submitted.connect(self.model.print_income)
-        self.model.error.connect(self.view.show_error)
+        self.storm_view.submitted.connect(self.model.set_data)
+        self.storm_view.start_STORM.connect(self.sim_STORM_thread.start)
+        self.storm_view.start_STORM.connect(self.on_started)
+        self.storm_view.submitted.connect(self.model.print_income)
+        self.model.error.connect(self.storm_view.show_error)
 
         self.model.finished.connect(self.on_finished)
 
+        self.storm_view.cancel_STORM.connect(self.on_cancel)
         # End main UI code
         self.show()
 
     def on_started(self):
-        # self.view.start_btn.setEnabled(False)
+        # self.storm_view.start_btn.setEnabled(False)
         self.start_STORM_sim.emit('Simulating blinking clusters.')
 
     def on_finished(self):
         # self.statusBar().showMessage('Simulation finished.')
         self.sim_STORM_thread.quit()
         self.sim_STORM_thread.deleteLater()
-        self.view.start_btn.setEnabled(True)
+        self.storm_view.start_btn.setEnabled(True)
         self.finished_STORM_sim.emit('Simulation finished.')
+
+    def on_cancel(self):
+        self.sim_STORM_thread.quit()
+        self.sim_STORM_thread.deleteLater()
+        self.storm_view.start_btn.setEnabled(True)
+        self.finished_STORM_sim.emit('STORM simulation cancelled.')
 
 # if __name__ == '__main__':
 #     app = qtw.QApplication(sys.argv)
