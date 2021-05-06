8c41011dc268082cacb56fce4396350805658541
SasLeo <saskia.kutz@fu-berlin.de>
2021-05-06 15:23:40 +0200

    renaming STORM to SMLM in file and class name

--- sim/model_simulation_module_smlm.py
+++ sim/model_simulation_module_smlm.py
@@ -3,7 +3,7 @@ from PyQt5 import QtCore as qtc
 from pythonr.Python_to_R import PythonToR
 
 
-class Model_STORM(qtc.QObject):
+class ModelSMLM(qtc.QObject):
     error = qtc.pyqtSignal(str)
     finished = qtc.pyqtSignal()
 
