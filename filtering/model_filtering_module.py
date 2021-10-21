e1cc076c56364a85c4d21b86605f58b3586b8191
SasLeo <saskia.kutz@fu-berlin.de>
2021-10-21 16:52:24 +0200

    check correct directory

--- filtering/model_filtering_module.py
+++ filtering/model_filtering_module.py
@@ -15,9 +15,26 @@ class ModuleFiltering(qtw.QWidget):
         super().__init__()
         self.inputs = None
 
-    @qtc.pyqtSlot(object)
+    @qtc.pyqtSlot(str)
     def set_data(self, inputs):
         self.inputs = inputs
+
+    @qtc.pyqtSlot()
+    def print_income(self):
+        """check for correct directory and connection to R"""
+
+        print("save_called")
+
+        error = ''
+        dir_ = self.inputs
+
+        if dir_ == "Select file":
+            error = f'You need to choose a file'
+        elif not path.isdir(dir_.rsplit('/', 1)[0]):
+            error = f'You need to choose a valid directory'
+
+        if error:
+            self.error.emit(error)
 
     def import_data(self):
         pass
