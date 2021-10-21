4832ccf45981800615f3103b1c9be09925b69a9c
SasLeo <saskia.kutz@fu-berlin.de>
2021-10-21 16:44:13 +0200

    set_data

--- filtering/model_filtering_module.py
+++ filtering/model_filtering_module.py
@@ -9,9 +9,15 @@ from PyQt5 import QtWidgets as qtw
 
 class ModuleFiltering(qtw.QWidget):
 
+    error = qtc.pyqtSignal(str)
+
     def __init__(self):
         super().__init__()
         self.inputs = None
+
+    @qtc.pyqtSlot(object)
+    def set_data(self, inputs):
+        self.inputs = inputs
 
     def import_data(self):
         pass
