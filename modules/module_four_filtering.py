735a9a158202f20241bde35f95a705e328b2a51b
SasLeo <saskia.kutz@fu-berlin.de>
2021-10-22 10:25:04 +0200

    connect view_filtering_module.py and model_filtering_module.py

--- modules/module_four_filtering.py
+++ modules/module_four_filtering.py
@@ -7,7 +7,7 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 
 from filtering.view_filtering_module import ViewFiltering
-# from post.model_post_module import ModelPost
+from filtering.model_filtering_module import ModuleFiltering
 
 
 class MainWindowFiltering(qtw.QWidget):
@@ -19,8 +19,12 @@ class MainWindowFiltering(qtw.QWidget):
         # Main UI code goes here
 
         self.filtering_view = ViewFiltering()
+        self.filtering_model = ModuleFiltering()
         self.setLayout(qtw.QVBoxLayout())
         self.layout().addWidget(self.filtering_view)
+
+        self.filtering_view.sub_data.connect(self.filtering_model.set_data)
+
 
         # End main UI code
         self.show()
